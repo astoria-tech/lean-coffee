fa2c8ff394714e8d69f13ab829e6ea73857f9e9c
Blane Cordes <blane@replogic.com>
2019-02-08 23:41:35 -0500

    initial commit

---
+++ app/controllers/cards_controller.rb
@@ -0,0 +1,53 @@
+module Api::V1
+  class CardsController < ApplicationController
+    before_action :set_card, only: [:show, :update, :destroy]
+
+    # GET /cards
+    def index
+      @cards = Card.all
+
+      render json: @cards
+    end
+
+    # GET /cards/1
+    def show
+      render json: @card
+    end
+
+    # POST /cards
+    def create
+      @card = Card.new(card_params)
+
+      if @card.save
+        render json: @card, status: :created, location: @card
+      else
+        render json: @card.errors, status: :unprocessable_entity
+      end
+    end
+
+    # PATCH/PUT /cards/1
+    def update
+      if @card.update(card_params)
+        render json: @card
+      else
+        render json: @card.errors, status: :unprocessable_entity
+      end
+    end
+
+    # DELETE /cards/1
+    def destroy
+      @card.destroy
+    end
+
+    private
+      # Use callbacks to share common setup or constraints between actions.
+      def set_card
+        @card = Card.find(params[:id])
+      end
+
+      # Only allow a trusted parameter "white list" through.
+      def card_params
+        params.require(:card).permit(:title)
+      end
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :cards
+    end
+  end
+end
