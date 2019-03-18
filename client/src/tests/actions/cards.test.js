d822d83b1c5ef80beb941b7b6bdba26f8eb575db
DanielChen1234 <dchen8492@yahoo.com>
2019-03-17 22:18:47 -0400

    writing actions tests

---
+++ client/src/tests/actions/cards.test.js
@@ -0,0 +1,15 @@
+import mockFetch from '../fetch'
+import {getCards} from '../../actions/cards'
+
+it('performs a get request using fetch and returns a dispatch with an array of objects', async () => {
+    mockFetch.get.mockImplementationOnce(() => {
+        Promise.resolve({
+            data: {
+                results: [{card: 'this is a test', title: 'lean-coffee'}]
+            }
+        })
+    })
+
+    const actions = await getCards()
+    console.log(actions)
+})
